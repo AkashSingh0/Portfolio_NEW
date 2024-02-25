9f4e2b76a3fe801536ee94caefa7bf9a3d952d22
Rishabh Pandey <rishabhpandey8092@gmail.com>
2024-02-25 23:39:10 +0530

    Menu fix

---
+++ index.js
@@ -0,0 +1,55 @@
+let isMenuChecked = false;
+let navButton = document.querySelector("#check");
+let menuList = document.querySelector(".list");
+navButton.addEventListener("click", function(){
+    if(isMenuChecked == false)
+    {
+        menuList.style.left = "0%";
+        isMenuChecked = true;
+    }
+    else{
+        menuList.style.left = "-100%";
+        isMenuChecked = false;
+    }
+})
+
+menuList.addEventListener("click", (event) =>{
+    if(event.target.localName == "a" && isMenuChecked == true)
+    {
+        menuList.style.left = "-100%";
+        isMenuChecked = false;
+    }
+})
+
+
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+    function opentab(tabname) {
+    for (tablink of tablinks) {
+          tablink.classList.remove("active-link");
+        }
+        for (tabcontent of tabcontents) {
+          tabcontent.classList.remove("active-tab");
+        }
+        event.currentTarget.classList.add("active-link");
+        document.getElementById(tabname).classList.add("active-tab");
+      }
+
+      //var btn2=document.querySelector(".portfolio");
+      document.querySelector(".action__btns").addEventListener("click",function(e){
+        if(e.target.innerText=="Hire me" || e.target.innerText=="Resume"){
+          var link2=document.createElement("a");
+          if(e.target.innerText=="Hire me"){
+            link2.href="https://www.linkedin.com/in/akash-singh-818345222/";
+          }
+          else{
+            link2.href="https://drive.google.com/file/d/1DYRcAvGdqqCWGfdNtOqMkyNiMbXBAsIa/view?usp=drivesdk";
+            link2.download="portfolio";
+          }
+          link2.target="_blank";
+          link2.style.color="#fff";
+          link2.click();
+        } 
+});
